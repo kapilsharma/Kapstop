3003f620eee8b2ba0549bea4a0a182dad004504a
kapilsharma <kapil@kapilsharma.info>
2012-11-07 23:53:20 +0530

    till now

--- src/webapp/getapps.php
+++ src/webapp/getapps.php
@@ -11,13 +11,15 @@ $installedApps = array();
 
 $ktaAbout = array();
 $ktaAbout['title']="About";
-$ktaAbout['layout']="full";
+$ktaAbout['window']="iframe";
+$ktaAbout['icon16']='http://local.kapstop.com/webapp/apps/about/images/icon16x16.jpg';
 
 $installedApps['about']=$ktaAbout;
 
 $ktaMsg = array();
 $ktaMsg['title']="Msg";
-$ktaMsg['layout']="full";
+$ktaMsg['window']="iframe";
+$ktaMsg['icon16']="http://local.kapstop.com/webapp/apps/msg/images/icon16x16.png";
 
 $installedApps['msg']=$ktaMsg;
 
